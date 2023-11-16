49058f66c6f640c8bf2b7a59f37952294af5f83e
kimjiyun <faren02134@gmail.com>
2023-11-16 17:16:01 +0900

    345

---
+++ src/main/java/com/example/board/config/InterceptorConfig.java
@@ -0,0 +1,20 @@
+package com.example.board.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.board.interceptor.SignInCheckInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    private SignInCheckInterceptor signInCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(signInCheckInterceptor).addPathPatterns("/board/**");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
---
+++ src/main/java/com/example/board/model/Board.java
@@ -0,0 +1,23 @@
+package com.example.board.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String title;
+	private String content;
+
+	// private String userId;
+
+	@ManyToOne
+	User user;
+}
