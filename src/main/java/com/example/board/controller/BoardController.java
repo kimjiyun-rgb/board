76f2e831f34dc9b9a7173d68d2ff5a14da524e86
kimjiyun <faren02134@gmail.com>
2023-11-17 16:07:33 +0900

    345

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -153,6 +153,6 @@ public class BoardController {
 		atchFile.setBoard(savedBoard);
 		atchFileRepository.save(atchFile);
 
-		return "board/write";
+		return "redirect:/board/" + savedBoard.getId();
 	}
 }
