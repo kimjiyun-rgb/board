1cbb2df886073d86ef2adc49cd8926ee5f0d960f
kimjiyun <faren02134@gmail.com>
2023-11-17 10:31:23 +0900

    345

---
+++ src/main/java/com/example/board/model/Comment.java
@@ -0,0 +1,22 @@
+package com.example.board.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue
+    long id;
+    String comment;
+
+    @ManyToOne
+    Board board;
+
+    @ManyToOne
+    User user;
+}
