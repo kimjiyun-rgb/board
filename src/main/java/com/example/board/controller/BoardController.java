da1c5ae374e7eda64c2603b945cae6b421850822
kimjiyun <faren02134@gmail.com>
2023-11-17 10:50:30 +0900

    345

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
 
 import com.example.board.model.Board;
+import com.example.board.model.Comment;
 import com.example.board.model.User;
 import com.example.board.repository.BoardRepository;
+import com.example.board.repository.CommentRepository;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -26,6 +29,23 @@ public class BoardController {
 
 	@Autowired
 	HttpSession session;
+
+	@Autowired
+	CommentRepository commentRepository;
+
+	@PostMapping("/board/comment/add")
+	public String commentAdd(@ModelAttribute Comment comment, @RequestParam long boardId) {
+		// 1. board_id
+		Board board = new Board();
+		board.setId(boardId); // 댓글을 추가할 게시물의 번호로 Board 객체 생성
+		comment.setBoard(board);
+		// 2. user_id
+		User user = (User) session.getAttribute("user_info");
+		comment.setUser(user);
+
+		commentRepository.save(comment);
+		return ""; // 임시, 에러가 보이지 않도록
+	}
 
 	@GetMapping("/board/delete/{id}")
 	public String boardDelete(@PathVariable("id") long id) {
