ef2d679605c9117cc062c788a89cccf21e2e0b4c
kimjiyun <faren02134@gmail.com>
2023-11-23 15:08:27 +0900

    345

--- src/test/java/com/example/board/BoardApplicationTests.java
+++ src/test/java/com/example/board/BoardApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.board;
 
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -20,6 +21,13 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 @Slf4j
 class BoardApplicationTests {
+
+	@Test
+	void stringToBase64() {
+		String text = "jwt test";
+		String encodedString = Base64.getEncoder().encodeToString(text.getBytes());
+		log.info("encodedString {}", encodedString);
+	}
 
 	@Test
 	void createJwt() {
